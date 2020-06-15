57b20f68eda92cd64e2d3d0e3aa75321f8c10f41
Hazel_K <59563487+Hazel-K@users.noreply.github.com>
2020-06-15 17:51:00 +0900

    200615 Lesson Complete

---
+++ first/src/blog/hyojin4588/first/mission/Mission6_6.java
@@ -0,0 +1,18 @@
+package blog.hyojin4588.first.mission;
+
+public class Mission6_6 {
+
+	public static void main(String[] args) {
+
+		int star = 6;
+
+		for (int i = 0; i < star; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
