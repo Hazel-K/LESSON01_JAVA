d1caaa8350b32be2980f9f1e92185b19cafec6b6
Hazel_K <59563487+Hazel-K@users.noreply.github.com>
2020-06-15 19:43:57 +0900

    200615 Lesson_Complete

---
+++ first/src/blog/hyojin4588/first/HelloWorld.java
@@ -0,0 +1,9 @@
+package blog.hyojin4588.first;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("헬로우 월드!!");
+		System.out.print("헬로우\n");
+		System.out.print("월드!!");
+	}
+}
---
+++ first/src/blog/hyojin4588/first/Variable3.java
@@ -0,0 +1,14 @@
+package blog.hyojin4588.first;
+
+public class Variable3 {
+	public static void main(String[] args) {
+		int n1 = 10;
+		long n4 = 222222;
+		float n2 = n4;
+		double n3 = n2;
+		int result = (int)n2 + n1;
+		
+		System.out.println(n1);
+		System.out.println(n2);
+	}
+}
