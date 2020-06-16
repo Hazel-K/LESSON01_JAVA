493c62f6f96edb795f2e9f8df46680c044717d01
Hazel_K <59563487+Hazel-K@users.noreply.github.com>
2020-06-16 17:53:16 +0900

    200616 수업 끝

--- first/src/blog/hyojin4588/first/level2/ArrayOrder2.java
+++ first/src/blog/hyojin4588/first/level2/ArrayOrder2.java
@@ -10,25 +10,25 @@ public class ArrayOrder2 {
 
 		// 선택정렬
 		for (int i = 0; i < arr.length - 1; i++) { // 0 - 7
-			
+
 			iMin = i; // 앞줄 재검색 방지
-			
+
 			for (int j = i + 1; j < arr.length; j++) { // 1 - 8
-				
+
 				if (arr[iMin] > arr[j]) { // 바뀐 부분 재검색 방지
 					iMin = j;
-					
+
 					// System.out.println(iMin); // 확인용 출력구문
 				} // if
-				
+
 			} // for j
 
 			iTemp = arr[i];
 			arr[i] = arr[iMin];
-			arr[iMin] = iTemp; 
-			
+			arr[iMin] = iTemp;
+
 		} // for i
-		
+
 		for (int k : arr) {
 			System.out.print(k + " ");
 		} // for k
