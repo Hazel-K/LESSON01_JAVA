60a7cc875243aada611b9fdb03a3db49e0d3f354
Hazel_K <59563487+Hazel-K@users.noreply.github.com>
2020-06-17 20:43:08 +0900

    GreatMission modify

--- first/src/blog/hyojin4588/first/mission/GreatMission3.java
+++ first/src/blog/hyojin4588/first/mission/GreatMission3.java
@@ -54,6 +54,44 @@ public class GreatMission3 {
 		System.out.printf("국어 합계 : %,d점, 평균 : %.1f점\n", iKor, iKorAvg);
 		System.out.printf("전체 합계 : %,d점, 평균 : %.1f점\n", iSum, iSumAvg);
 		
+		// 유연한 방식.
+		/*
+		int[][] arr = {
+				{90, 100, 88}, //영어
+				{34, 99, 45}, //수학
+				{98, 65, 87} //국어
+		}; // Array 정렬
+		
+		String[] clsArr = {"영어", "수학", "국어"}; // 과목 Arr 정렬
+		int[] sumArr = new int[clsArr.length]; // sumArr 1차배열은 clsArr.length 만큼의 크기를 가지는 배열이다.
+		
+		// 각 항목 누적합 산출
+		// 확인용
+		// System.out.println(arr.length); 결과값 = 3 (배열{ }이 세 개)
+		// System.out.println(arr[i].length); 결과값 = 3 (값이 세 개, 90, 100, 88 등)
+		for(int i=0; i<arr.length; i++) {
+			for(int z=0; z<arr[i].length; z++) {
+				sumArr[i] += arr[i][z]; // clsArr[i] 항목에 있는 것들 전부 누적합 시키겠다는 의미
+			}
+		}
+		// 각 항목 누적합 산출 종료
+		
+		int totalSum = 0; // 합계 누적용 변수
+		int totalCnt = 0; // 전체평균 수집용 변수
+		
+		// 각 과목명 및 합계와 평균 동시 출력
+		// 확인용
+		// System.out.println(sumArr.length); 결과값 = 3 ()
+		for(int i=0; i<sumArr.length; i++) {
+			System.out.printf("%s합계: %d, 평균: %.1f\n"
+					, clsArr[i], sumArr[i], (float)sumArr[i]/arr[i].length);
+			totalSum += sumArr[i];
+			totalCnt += arr[i].length; // 낱개의 element를 하나하나 세지 않고 한 줄에 들어 있는 자식들의 값을 누적합시키는 점이 인상적임
+		}
+		// 각 과목명 및 합계와 평균 동시 출력
+		
+		System.out.printf("전체합계: %d, 평균: %.1f\n", totalSum, (float)totalSum/totalCnt); // 전체합계 출력구문
+		*/
 	}
 
 }
