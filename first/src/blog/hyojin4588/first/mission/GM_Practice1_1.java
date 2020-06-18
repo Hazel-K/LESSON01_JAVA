3b197db4378b83fb493c18b29a124136dc3c2de1
Hazel_K <59563487+Hazel-K@users.noreply.github.com>
2020-06-18 09:06:15 +0900

    modify

--- first/src/blog/hyojin4588/first/mission/GM_Practice1_1.java
+++ first/src/blog/hyojin4588/first/mission/GM_Practice1_1.java
@@ -36,7 +36,7 @@ public class GM_Practice1_1 {
 		for (int i = 0; i < classArr.length; i++) {
 			for (int j = 0; j < scoreArr[i].length; j++) {
 				classSum[i] += scoreArr[i][j]; // 각 과목의 합이 누적합으로서 배열에 삽입되므로 classArr 길이만큼의 공간이 필요하다.
-				classCount++;
+				classCount++; // 전체 평균 산출 시 필요한 요소이므로 2중 for문이 반복 수행될 때마다 개수를 누적시킨다.
 			}
 			classTotal += classSum[i];
 			classAvg[i] = (float)classSum[i] / scoreArr[i].length;// 2중 for문 안에서 실행 시 3번씩 값이 정해지므로 넣을 필요가 없음
