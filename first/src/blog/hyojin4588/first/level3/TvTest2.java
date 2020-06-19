a0641a0168da71c42f8546ca47830233e17b1b59
Hazel_K <59563487+Hazel-K@users.noreply.github.com>
2020-06-19 16:49:06 +0900

    객체 수업 예제들

--- first/src/blog/hyojin4588/first/level3/TvTest2.java
+++ first/src/blog/hyojin4588/first/level3/TvTest2.java
@@ -6,6 +6,7 @@ public class TvTest2 {
 		
 		Tv tv1 = new Tv();
 		Tv tv2 = tv1; // tv1, tv2 두 변수가 가리키는 곳은 똑같다.
+		// tv1의 주소값을 복사하여 tv2에다가 붙여주는 셈.
 		
 		tv1.channalUp();
 		tv2.channalUp();
---
+++ first/src/blog/hyojin4588/first/level3/TvTest3.java
@@ -0,0 +1,14 @@
+package blog.hyojin4588.first.level3;
+
+public class TvTest3 {
+	
+	public static void main(String[] args) {
+		
+		Tv[] tvArr = new Tv[3]; // tvArr = { Tv0, Tv1, Tv2 };
+		tvArr[0] = new Tv();
+		tvArr[1] = new Tv();
+		tvArr[2] = new Tv();
+		
+	}
+
+}
