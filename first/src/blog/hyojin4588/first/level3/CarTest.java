ca06efdbb3922054dbe42907dcf589caf51503ed
Hazel_K <59563487+Hazel-K@users.noreply.github.com>
2020-06-22 10:12:39 +0900

    생성자 수업 보충

--- first/src/blog/hyojin4588/first/level3/CarTest.java
+++ first/src/blog/hyojin4588/first/level3/CarTest.java
@@ -2,14 +2,18 @@ package blog.hyojin4588.first.level3;
 
 public class CarTest {
 	
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		Car car = new Car();
 		Car car1 = new Car("소나타", "흰색", 2500);
+		Car car2 = new Car("그랜저");
+		Car car3 = new Car("G80");
 		// car1.name = "소나타"; // 생성자를 쓰기 전 데이터 입력 방식
 		// car1.color = "흰색"; // 생성자를 쓰기 전 데이터 입력 방식
 		
 		car.introduceMyCar();
 		car1.introduceMyCar();
+		car2.introduceMyCar();
+		car3.introduceMyCar();
 		System.out.println("---");
 	}
 
