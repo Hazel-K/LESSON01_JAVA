ae52c888400428c48310ba515b291239b0d33715
Hazel_K <59563487+Hazel-K@users.noreply.github.com>
2020-06-23 10:15:10 +0900

    객체 예제 4 보충

--- first/src/blog/hyojin4588/first/level3/BoardVO.java
+++ first/src/blog/hyojin4588/first/level3/BoardVO.java
@@ -9,6 +9,7 @@ public class BoardVO {
 	
 	// Constructor
 	public BoardVO() {} // 기본생성자, 하나도 없을 때는 컴파일러가 자동으로 삽입
+	// 기본 생성자와 세터 지정을 하지 않으면 인자를 활용한 생성자를 통해 객체를 상수처럼 활용 가능
 	
 	public BoardVO(String title, String content, int writeID) {
 		super();
