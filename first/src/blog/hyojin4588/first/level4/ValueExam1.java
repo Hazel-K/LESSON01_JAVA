3951cd3386e5783c7e65bc08c3cccfe1563f4a9e
Hazel_K <59563487+Hazel-K@users.noreply.github.com>
2020-06-26 12:51:15 +0900

    상속 설명 추가

---
+++ first/src/blog/hyojin4588/first/level4/ValueExam1.java
@@ -0,0 +1,20 @@
+package blog.hyojin4588.first.level4;
+
+public class ValueExam1 {
+
+	private int val;
+
+	public ValueExam1(int val) {
+		this.val = val;
+	}
+
+	public int getVal() {
+		return val;
+	}
+/*
+	@Override
+	public boolean equals(Object i) {
+		return Integer.toString(val) == i; 
+	}
+*/
+}
---
+++ first/src/blog/hyojin4588/first/level4/ValueTest1.java
@@ -0,0 +1,21 @@
+package blog.hyojin4588.first.level4;
+
+public class ValueTest1 {
+	
+	public static void main(String[] args) {
+		String str1 = new String("A");
+		String str2 = new String("A");
+		
+		System.out.println("1: " + (str1 == str2));
+		System.out.println("2: " + str1.equals(str2));
+		// 자기 걸 오버라이딩해서 쓰고 있음.
+		
+		ValueExam1 val1 = new ValueExam1(1);
+		ValueExam1 val2 = new ValueExam1(2);
+		
+		System.out.println("1: " + (val1 == val2));
+		System.out.println("2: " + val1.equals(val2));
+		// 자기 걸 오버라이딩 하지 않고 있음
+	}
+
+}
