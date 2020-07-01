295e51badbf624451962ae788e867d1be3845f02
Hazel_K <59563487+Hazel-K@users.noreply.github.com>
2020-07-01 11:05:21 +0900

    인터페이스 및 상속 예제

--- first/src/blog/hyojin4588/first/level7/Medic.java
+++ first/src/blog/hyojin4588/first/level7/Medic.java
@@ -1,20 +1,20 @@
 package blog.hyojin4588.first.level7;
 
-public class Medic extends Unit implements Treatable{
-	
+public class Medic extends Unit implements Treatable {
+
 	public Medic() {
 		super("메딕", 70);
 	}
-	
+
 	public void heal(Unit u) {
-		if (u instanceof Treatable) {
+		if (!(u instanceof Treatable) || this == u) {
+			System.out.println("이 유닛을 치료할 수 없습니다.");
+		} else {
 			System.out.println("히이일");
-			while(u.getCurrent_hp() < u.MAX_HP) {
+			while (u.getCurrent_hp() < u.MAX_HP) {
 				u.setCurrent_hp(1);
 			}
-		} else {
-			System.out.println("이 유닛을 치료할 수 없습니다.");
 		}
 	}
-
+	
 }
